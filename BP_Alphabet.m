ffc67f2a3b0fbdd19d5d45c6e9ae860cecd7df51
NLGRF <nongrace6654@gmail.com>
2017-10-26 15:06:29 +0700

    Edit Num

--- BP_Alphabet.m
+++ BP_Alphabet.m
@@ -8,21 +8,18 @@
 
 net=newff(minmax(Al),[1 36],{'logsig','purelin'},'traingdx');
 net.trainParam.epochs=1000
-net.trainParam.goal=0.001
+net.trainParam.goal=0.0001
 net.trainParam.show=1
 
 %train
 net = train(net,Al,T);
 
-%test for B with the same training data
+%test for Seg with the same training data
 output=sim(net,Al(:,36));
 [m,A_Id]=max(output)
 
-B = Al(:,36);
-plotchar(B)
-%test for Z with the same training data
-output=sim(net,Al(:,36));
-[m,A_Id]=max(output)
+Seg = Al(:,36);
+plotchar(Seg)
 
 %test for A with noise generated
 %Anoise=Al(:,6)+0.20*randn(size(Al(:,6)));
